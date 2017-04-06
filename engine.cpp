89729e93369857c02479aa08819a2dd868ebbd59
dmitry_leontiev <dm.leontiev7@gmail.com>
2017-04-06 17:46:36 +0300

    implemented simple http server: QDesktopServices cannot open file://urls with queries

--- engine.cpp
+++ engine.cpp
@@ -1,103 +1,50 @@
 #include "engine.h"
+#include "dialog.h"
+#include <stdio.h>
 #include <QApplication>
 #include <QDesktopServices>
-#include <QDialog>
-#include <QGridLayout>
-#include <QGroupBox>
-#include <QLabel>
-#include <QLineEdit>
-#include <QCheckBox>
+#include <QFile>
+#include <QDebug>
 
-#include <QComboBox>
-
-
-class ConfDialog : public QDialog {
-public:
-    ConfDialog(QSettings& _settings, Engine * e)
-        : settings(_settings)
-    {
-        QGridLayout * layout = new QGridLayout();
-        QGroupBox   * lj_box = new QGroupBox(tr("Livejournal"));
-        QGroupBox   * db_box = new QGroupBox(tr("Database"));
-        QGroupBox   * cn_box = new QGroupBox(tr("Connectivity"));
-        QGroupBox   * ab_box = new QGroupBox(tr("About"));
-        layout->addWidget(lj_box, 0, 0);
-        layout->addWidget(db_box, 0, 1);
-        layout->addWidget(cn_box, 1, 0);
-        layout->addWidget(ab_box, 1, 1);
-        QDialog::setLayout(layout);
-
-        layout = new QGridLayout(lj_box);
-        QLabel      * l0 = new QLabel(tr("<h2>Warning!</h2><br>"
-                                         "your password will be stored as<br>"
-                                         "plain text. Do not enter password<br>"
-                                         "if you are not confident in PC you<br>"
-                                         "are working on"));
-        layout->addWidget(l0, 0, 0, 1, 2);
-        QLabel      * l1 = new QLabel(tr("Livejournal login"));
-        layout->addWidget(l1, 1, 0, 1, 1, Qt::AlignRight);
-        QLineEdit   * e1 = new QLineEdit();
-        e1->setText(settings.value("lj_user").toString());
-        e1->setMinimumWidth(200);
-        layout->addWidget(e1, 1, 1, 1, 1, Qt::AlignLeft);
-        QLabel      * l2 = new QLabel(tr("Password"));
-        layout->addWidget(l2, 2, 0, 1, 1, Qt::AlignRight);
-        QLineEdit   * e2 = new QLineEdit();
-        e2->setText(settings.value("lj_passwd").toString());
-        e2->setEchoMode(QLineEdit::PasswordEchoOnEdit);
-        e2->setMinimumWidth(200);
-        layout->addWidget(e2, 2, 1, 1, 1, Qt::AlignLeft);
-        lj_user = e1;
-        lj_passwd = e2;
-
-
-    }
-    int exec() {
-        int result = QDialog::exec();
-        if (result == QDialog::Accepted) {
-            settings.setValue("lj_user", lj_user->text());
-            settings.setValue("lj_passwd", lj_passwd->text());
-            settings.sync();
-        } else {
-            lj_user->setText(settings.value("lj_user").toString());
-            lj_passwd->setText(settings.value("lj_passwd").toString());
-        }
-        return result;
-    }
-private:
-    QSettings& settings;
-    QLineEdit* lj_user;
-    QLineEdit* lj_passwd;
-};
 
 Engine::Engine(QSettings &_settings, QObject *parent)
     : QObject(parent)
     , settings(_settings)
     , dialog(new ConfDialog(_settings, this))
-    , server(QString("lj.offline"), QWebSocketServer::NonSecureMode)
+    , ws_server(QString("lj.offline"), QWebSocketServer::NonSecureMode)
 {
     messages_enabled = settings.value("popup").toBool();
 
     if (!server.listen(QHostAddress::LocalHost)) {
+        qDebug()<<"Failed to start HTTP server: "<<server.errorString();
         last_error = server.errorString();
         return;
     }
-    connect(&server, SIGNAL(newConnection()), this, SLOT(newConnection()));
+    connect(&server, SIGNAL(newConnection()), this, SLOT(newHttpReq()));
+    server_path = qApp->applicationDirPath()+"/web/";
+
+    if (!ws_server.listen(QHostAddress::LocalHost)) {
+        qDebug()<<"Failed to start websocket server: "<<server.errorString();
+        last_error = ws_server.errorString();
+        return;
+    }
+    connect(&ws_server, SIGNAL(newConnection()), this, SLOT(newConnection()));
 
     index_html = QUrl(
-        "file://"+
-        qApp->applicationDirPath()+
-        "/web/index.html");
-    index_html.setQuery("server=localhost:"+QString::number(server.serverPort()));
-
-    reopenDatabase();
+        "http://localhost:"+QString::number(server.serverPort())+"/index.html");
+    index_html.setQuery(QString::number(ws_server.serverPort()));
 }
 
-void Engine::reopenDatabase() {
+Engine::~Engine() {
+    emit killConnections();
 }
-
 void Engine::showConfigDialog() {
-    dialog->exec();
+    if (dialog->exec() == QDialog::Accepted) {
+        emit killConnections();
+
+        delete backend;
+        backend = dialog->createBackend();
+    }
 }
 
 void Engine::enableMessages(bool on) {
@@ -118,13 +65,182 @@ void Engine::syncronize() {
 
 }
 
-void Engine::newConnection() {
+
+void Engine::newHttpReq() {
     if (!server.hasPendingConnections())
         return;
 
-    QWebSocket * socket = server.nextPendingConnection();
+    QTcpSocket * socket = server.nextPendingConnection();
     if (!socket)
         return;
 
-    SocketWrapper * wrapper = new SocketWrapper(socket, this);
+    connect(socket, SIGNAL(disconnected()), this, SLOT(httpConnClosed()));
+    connect(socket, SIGNAL(readyRead()), this, SLOT(httpDataReceived()));
+
+    if (socket->bytesAvailable())
+        httpDataReceived(socket);
+}
+
+void Engine::httpDataReceived() {
+    QTcpSocket * socket = qobject_cast<QTcpSocket*>(sender());
+    if (!socket) return;
+    httpDataReceived(socket);
+}
+
+void Engine::httpDataReceived(QTcpSocket *socket) {
+    if (!socket->canReadLine())
+        return;
+
+    const int L = 1024;
+    char buffer[L];
+
+
+    do {//reading first line of request
+        qint64 len = socket->readLine(buffer, L);
+        if (len < 1 || len >= L)
+            break;
+        buffer[L-1] = 0;
+
+        //empty line received, find and send file
+        if (buffer[0]=='\n') {
+            QVariant v = socket->property("url");
+            if (v.isNull())
+                break;
+
+            QString newpath = server_path + v.toString();
+            if (!QFile::exists(newpath)) {
+                socket->write("HTTP/1.0 404 not found\r\n\r\n");
+                return;
+            }
+            QFile file(newpath);
+            if (!file.open(QIODevice::ReadOnly)) {
+                socket->write("HTTP/1.0 403 forbidden\r\n\r\n");
+                return;
+            }
+            if (file.size() > 256*1024) {
+                socket->write("HTTP/1.0 600 file too large\r\n\r\n");
+                return;
+            }
+            QByteArray contents = file.readAll();
+
+            char buffer[10];
+            snprintf(buffer, 10, "%d", contents.size());
+
+            socket->write("HTTP/1.0 200 OK\r\n"
+                          "Content-Length: ");
+            socket->write(buffer);
+            if (newpath.endsWith(".html"))
+                socket->write("\r\nContent-Type: text/html\r\n\r\n");
+            else
+            if (newpath.endsWith(".js"))
+                socket->write("\r\nContent-Type: application/javascript\r\n\r\n");
+            else
+            if (newpath.endsWith(".css"))
+                socket->write("\r\nContent-Type: text/css\r\n\r\n");
+            else
+            if (newpath.endsWith(".png"))
+                socket->write("\r\nContent-Type: image/png\r\n\r\n");
+            else
+                socket->write("\r\nContent-Type: text/plain\r\n\r\n");
+            socket->write(contents);
+            socket->setProperty("url", QVariant());
+            return;
+        }
+
+        char * next;
+        if ((0x20|buffer[0]) == 'g' &&
+            (0x20|buffer[1]) == 'e' &&
+            (0x20|buffer[2]) == 't' &&
+            buffer[3] == ' ')
+        {
+            next   = buffer+4;
+        }
+        else break;
+
+        while((next < buffer+L) && (*next == ' '))
+            ++next;
+
+        if (next >= buffer+L)
+            break;
+
+        if ((0x20|next[0]) == 'h' &&
+            (0x20|next[1]) == 't' &&
+            (0x20|next[2]) == 't' &&
+            (0x20|next[3]) == 'p' &&
+            next[4] == ':' &&
+            next[5] == '/' &&
+            next[6] == '/')
+        {
+            next += 7;
+            //skip hostname and port, if any
+            for(;;) {
+                char cc = *next;
+                if (!cc || cc == '/' || isspace(cc))
+                    break;
+                ++next;
+            }
+
+            if (isspace(*next)) {
+                //assume index.html
+                socket->setProperty("url", QString("index.html"));
+                return;
+            }
+
+            if (*next != '/')
+                break;
+        }
+
+        //if we start from '/', cut it away
+        if (*next == '/')
+            ++next;
+
+        //safety checks: we have only one '.' in string(prevents ..)
+        //we start with have_1_dot = false;
+        //first time we see '.' we set it to true.
+        //if we see another '.', and have_1_dot == true, we set it to false & break;
+        bool have_1_dot = false;
+        for(char * c = next; *c; ++c) {
+            //normal browser won't send us spaces before HTTP/1.x
+            //we also stop on non-ascii characters, any whitespace character
+            //or control symbols(00-1f)
+            char cc = *c;
+            if (cc <= 0x20 || cc & 0x80 || isspace(cc)) {
+                *c = 0;
+                break;
+            }
+            bool dot = (*c) == '.';
+            if (have_1_dot & dot) {
+                have_1_dot = false;
+                break;
+            }
+            have_1_dot |= dot;
+        }
+        if (!have_1_dot)
+            break;
+        if (*next == 0)
+            break;
+
+        socket->setProperty("url", QString::fromLatin1(next));
+        return;
+    } while(0);
+    socket->close();
+    socket->deleteLater();
+}
+
+void Engine::httpConnClosed() {
+    QTcpSocket * socket = qobject_cast<QTcpSocket*>(sender());
+    if (!socket) return;
+    socket->deleteLater();
+}
+
+void Engine::newConnection() {
+    if (!ws_server.hasPendingConnections())
+        return;
+
+    QWebSocket * socket = ws_server.nextPendingConnection();
+    if (!socket)
+        return;
+
+    SocketWrapper * wrapper = new SocketWrapper(socket, backend, this);
+    connect(this, SIGNAL(killConnections()), wrapper, SLOT(killConnection()));
 }
--- engine.h
+++ engine.h
@@ -4,39 +4,50 @@
 #include <QSettings>
 #include <QSystemTrayIcon>
 #include <QObject>
+#include <QTcpServer>
 #include <QWebSocketServer>
 #include <QWebSocket>
 #include <QUrl>
-#include <QSqlDatabase>
 #include "socketwrapper.h"
+#include "backend.h"
+
+class ConfDialog;
 
 class Engine : public QObject
 {
     Q_OBJECT
 public:
     explicit Engine(QSettings& settings, QObject *parent = 0);
+    ~Engine();
     bool ok() const;
 signals:
     void message(const QString& title, const QString& message,QSystemTrayIcon::MessageIcon,int);
+    void killConnections();
 public slots:
     void enableMessages(bool on);
     void syncronize();
     void showInBrowser();
     void showConfigDialog();
 private slots:
+    void newHttpReq();
     void newConnection();
+    void httpConnClosed();
+    void httpDataReceived();
 private:
     QSettings&       settings;
     QString          last_error;
     bool             messages_enabled;
 
-    QDialog        * dialog;
+    ConfDialog     * dialog;
 
     QUrl             index_html;
-    QWebSocketServer server;
+    QTcpServer       server;
+    QString          server_path;
 
-    QSqlDatabase     database;
-    void reopenDatabase();
+    QWebSocketServer ws_server;
+
+    Backend        * backend = nullptr;
+    void httpDataReceived(QTcpSocket * socket);
 };
 
 #endif // ENGINE_H
